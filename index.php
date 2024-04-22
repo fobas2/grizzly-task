4b431d9cc734a821aea72c1fb7d83b1904481baa
fobas2 <fobas2@outlook.com>
2024-04-22 22:16:03 +0300

    Задание 2 - выполнено

--- index.php
+++ index.php
@@ -58,13 +58,51 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Определение страны по номеру телефона</title>
+    <title>Grizzly Digital - ТЗ Азявчиков Г.М.</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        /* Стили для попапа */
+        .cookie-popup {
+            position: fixed;
+            right: 20px;
+            bottom: 20px;
+            background-color: #f8f9fa;
+            padding: 10px;
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+            display: none;
+        }
+    </style>
 </head>
 <body>
 <form action="" method="post">
     <label for="phone"><b>Введите номер телефона:<b/></label>
     <input type="text" id="phone" name="phone" placeholder="+375(29)123-45-67" required>
-    <button type="submit">Определить страну</button>
+    <button class="btn btn-primary" type="submit">Определить страну</button>
 </form>
+<div class="cookie-popup" id="cookiePopup">
+    На этой странице используются куки. Нажмите "Принять", чтобы закрыть это уведомление.
+    <button class="btn btn-primary" onclick="acceptCookies()">Принять</button>
+    <button class="btn btn-secondary" onclick="closePopup()">Закрыть</button>
+</div>
+<script>
+    // Проверяем, был ли попап уже показан сегодня
+    const lastPopupDate = localStorage.getItem('lastPopupDate');
+    const today = new Date().toLocaleDateString();
+    if (lastPopupDate !== today) {
+        document.getElementById('cookiePopup').style.display = 'block';
+    }
+
+    // Закрытие попапа
+    function closePopup() {
+        document.getElementById('cookiePopup').style.display = 'none';
+    }
+
+    // Принятие информации о куки
+    function acceptCookies() {
+        closePopup();
+        localStorage.setItem('lastPopupDate', today);
+    }
+</script>
 </body>
 </html>
